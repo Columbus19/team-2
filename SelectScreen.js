b01e44e49a3a70b869fc982d0dc8f347d5cf9c82
neelm88 <neelm88@gmail.com>
2019-10-18 22:58:38 -0400

    temp

---
+++ SelectScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider } from 'react-native-elements';
+import select_styles from './Styles'
+
+export default function SelectScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={select_styles.header}>
+        <Text style={select_styles.header_text}>Who are you?</Text>        
+      </View>
+      <View style={select_styles.view}>
+        <Button title="Student" style={select_styles.buttons}></Button>
+      </View>
+      <View style={select_styles.view}>
+        <Button title="Alumni" style={select_styles.buttons}></Button>
+      </View>
+      <View style={select_styles.view}>
+        <Button title="Client" style={select_styles.buttons}></Button>
+      </View>
+    </View>
+  );
+}
+
+const theme = {
+  Button: {
+    titleStyle: {
+      color: 'red',
+    },
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
