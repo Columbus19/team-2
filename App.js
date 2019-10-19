ba3d630e0b0d547eb9f5623893a25665ee985e47
neelm88 <neelm88@gmail.com>
2019-10-18 21:58:08 -0400

    Added select screen

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, ThemeProvider } from 'react-native-elements';
+import select_styles from './Styles'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button> Next </Button>
+      <View style={select_styles.header}>
+        <Text style={select_styles.header_text}>Who are you?</Text>        
+      </View>
+      <View style={select_styles.view}>
+        <Button title="Student" style={select_styles.buttons}></Button>
+      </View>
+      <View style={select_styles.view}>
+        <Button title="Alumni" style={select_styles.buttons}></Button>
+      </View>
+      <View style={select_styles.view}>
+        <Button title="Client" style={select_styles.buttons}></Button>
+      </View>
     </View>
   );
 }
