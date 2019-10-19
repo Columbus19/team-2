472912d4efc819e523a78078b9c0225013ee81da
Reece <partridge.48@osu.edu>
2019-10-19 00:33:16 -0400

    homepage added

---
+++ Screens/LearningAcademy.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+import Constants from 'expo-constants';
+
+function Separator() {
+  return <View style={styles.separator} />;
+}
+
+export default function App() {
+  return (
+    
+    <SafeAreaView style={styles.container}>
+      <Text> 
+      Learning Academy
+      </Text>
+      <View>
+        <Button
+          title="Application"
+          color="#f194ff"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Button
+          title="Training Session"
+          color="#f194ff"
+          onPress={() => Alert.alert('Button with adjusted color pressed')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Button
+          title="Mock Interviews"
+          color="#f194ff"
+          onPress={() => Alert.alert('Cannot press this one')}
+        />
+      </View>
+      <Separator />
+
+        <View style={styles.title}>
+          <Button
+            title="Resume Template and Review"
+            color="#f194ff"
+            onPress={() => Alert.alert('Left button pressed')}
+          />
+        </View>
+        
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
