0752f0842967aa7d4e2eaa61683c1281d22ca699
Sim Singh <jeety12345@gmail.com>
2019-10-19 07:19:26 -0400

    Add client display

---
+++ assets/flatListData.js
@@ -0,0 +1,45 @@
+var flatListData = [
+    {
+        "key": "598a678278fee204ee51cd2c",
+        "name": "Agustin S. Baker",   
+        "imageUrl": "https://images.unsplash.com/photo-1547404415-5eb20ddab016?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",                    
+        "foodDescription": "GPA: 3.7 | Major: Information Technology | Location: Chicago, IL"
+    },
+    {
+        "key": "598a684f78fee204ee51cd2f",
+        "name": "Kevin S. Beard",        
+        "imageUrl": "https://images.unsplash.com/photo-1473492201326-7c01dd2e596b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",    
+        "foodDescription": "GPA: 3.9 | Major: Information Systems | Location: Columbus, OH"
+    },
+    {
+        "key": "598a687678fee204ee51cd30",
+        "name": "Nicole A. Page",
+        "imageUrl": "https://icon-library.net/images/default-user-icon/default-user-icon-4.jpg",
+        "foodDescription": "GPA: 3.5 | Major: Nursing | Location: Pittsburg, PA"
+    },
+    {
+        "key": "598a680178fee204ee51cd2e",
+        "name": "Barrett T. Smith",        
+        "imageUrl": "https://images.unsplash.com/photo-1494883759339-0b042055a4ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+        "foodDescription": "GPA: 3.7 | Major: Economics | Location: Tampa, FL"
+    },
+    {
+        "key": "598a688878fee204ee51cd31",
+        "name": "Janet E. Hunter",     
+        "imageUrl": "https://images.unsplash.com/photo-1464582883107-8adf2dca8a9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",   
+        "foodDescription": "GPA: 3.2 | Major: Marketing | Location: Plano, TX"
+    },
+    {
+        "key": "598a68b778fee204ee51cd32",
+        "name": "Thomas B. Perez",   
+        "imageUrl": "https://images.unsplash.com/photo-1564064695621-5cb08cd581eb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80",     
+        "foodDescription": "GPA: 3.3 | Major: Piano | Location: Dallas, TX"
+    },
+    {
+        "key": "598a67c478fee204ee51cd2d",
+        "name": "Anthony A. Morris",  
+        "imageUrl": "https://icon-library.net/images/default-user-icon/default-user-icon-4.jpg",      
+        "foodDescription": "GPA: 4.0 | Major: Biology | Location: New York, NY"
+    }
+  ];
+  module.exports = flatListData;
